c44d2561520149a9e80bdf8362ace8222c95b268
Sanket Barhate <sanketbarhate3@gmail.com>
2025-11-14 15:16:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,18 @@ import geminiResponse from "./gemini.js"
 
 
 const app=express()
+
+app.set("trust proxy", 1);
 app.use(cors({
-    origin:"https://syra-voice.vercel.app",
-    credentials:true
-}))
+    origin: [
+        "http://localhost:5173",
+        "https://syra-voice.vercel.app"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
