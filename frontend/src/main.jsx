a11a7ca0ffad238065420ad5c8ff0c85c80114ac
BarhateSanket <sanketbarhate3@gmail.com>
2025-11-17 14:54:00 +0530

    fix main.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
-import UserContext from './ContextApi/UserContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from './ContextApi/UserContext.jsx'
 import { ThemeProvider } from './ContextApi/ThemeContext.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
@@ -22,18 +22,17 @@ if ('serviceWorker' in navigator) {
 }
 
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-<ThemeProvider>
-<UserContext>
-  <div className="min-h-screen flex flex-col">
-    <Header />
-    <main className="flex-1">
-      <App />
-    </main>
-    <Footer />
-  </div>
-  </UserContext>
-  </ThemeProvider>
+  <BrowserRouter>
+    <ThemeProvider>
+      <UserProvider>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-1">
+            <App />
+          </main>
+          <Footer />
+        </div>
+      </UserProvider>
+    </ThemeProvider>
   </BrowserRouter>
-
 )
